a895089d5d6771020e6e3c824b059e99c7edbc73
Paweł Kownacki <koowal@gmail.com>
2020-05-07 15:25:50 +0100

    thursday

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+window.onscroll = function () { myFunction() };
 
 // Get the navbar
 let navbar = document.getElementById("sticky-menu");
